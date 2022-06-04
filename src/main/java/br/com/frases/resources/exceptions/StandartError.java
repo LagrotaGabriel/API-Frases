de90f9712c6ad6badba2b1eaa2982306e3b14570
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-04 03:51:22 -0300

    -> Finalizei a classe StandartError e todos os seus testes

--- src/main/java/br/com/frases/resources/exceptions/StandartError.java
+++ src/main/java/br/com/frases/resources/exceptions/StandartError.java
@@ -1,4 +1,19 @@
 package br.com.frases.resources.exceptions;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class StandartError {
+
+    private LocalDateTime localDateTime;
+    private Integer status;
+    private String error;
+    private String path;
+
 }
--- src/test/java/br/com/frases/resources/exceptions/StandartErrorTest.java
+++ src/test/java/br/com/frases/resources/exceptions/StandartErrorTest.java
@@ -1,10 +1,36 @@
 package br.com.frases.resources.exceptions;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import br.com.frases.mocks.StandartErrorBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 public class StandartErrorTest {
+
+    StandartError standartError;
+
+    @Test
+    @DisplayName("Should test StandartError setter methods")
+    public void shouldTestStandartErrorSetterMethods(){
+
+        Assertions.assertEquals("StandartError(localDateTime=2022-06-04T02:06:57.260990900, status=400, error=Erro de " +
+                "teste, path=/test)", StandartErrorBuilder.builder().withStaticTimeStamp().build().toString());
+
+    }
+
+    @Test
+    @DisplayName("Should test StandartError AllArgsConstructor")
+    public void shouldTestStandartErrorAllArgsConstructor(){
+
+        standartError = new StandartError(LocalDateTime.parse("2022-06-04T02:06:57.260990900"), 400,
+                "Erro de teste", "/test");
+
+        Assertions.assertEquals("StandartError(localDateTime=2022-06-04T02:06:57.260990900, status=400, error=Erro de " +
+                "teste, path=/test)", standartError.toString());
+    }
+
 }
