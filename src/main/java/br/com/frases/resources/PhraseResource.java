065981cd93e8e4bc1474a2bc48c54c462be32111
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-04 03:51:46 -0300

    -> Criei a estruturação básica inicial da classe resource

--- src/main/java/br/com/frases/resources/PhraseResource.java
+++ src/main/java/br/com/frases/resources/PhraseResource.java
@@ -1,7 +1,21 @@
 package br.com.frases.resources;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 @RestController
+@RequestMapping("api/phrase")
+@Produces({MediaType.APPLICATION_JSON, "application/json"})
+@Consumes({MediaType.APPLICATION_JSON, "application/json"})
+@Api(value = "O aplicativo tem como intuito realizar o cadastro, consulta, alteração e delete de frases")
 public class PhraseResource {
+
+
+
+
 }
