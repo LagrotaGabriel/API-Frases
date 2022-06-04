dd611bfb4c73ebbf05bc8c2011be828017fa0168
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-04 03:50:36 -0300

    -> Finalizei a classe de implementação do DAO

--- src/main/java/br/com/frases/services/dao/PhraseDAOImpl.java
+++ src/main/java/br/com/frases/services/dao/PhraseDAOImpl.java
@@ -1,7 +1,64 @@
 package br.com.frases.services.dao;
 
+import br.com.frases.config.ModelMapperConfig;
+import br.com.frases.models.dto.PhraseDTO;
+import br.com.frases.models.entity.PhraseEntity;
+import br.com.frases.repositories.PhraseRepository;
+import br.com.frases.resources.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class PhraseDAOImpl {
+public class PhraseDAOImpl implements PhraseDAO{
+
+    @Autowired
+    PhraseRepository phraseRepository;
+
+    @Autowired
+    ModelMapperConfig modelMapper;
+
+    @Override
+    public PhraseEntity create(PhraseDTO phraseDTO) {
+        return phraseRepository.save(modelMapper.mapper().map(phraseDTO, PhraseEntity.class));
+    }
+
+    @Override
+    public List<PhraseEntity> findAll() {
+        return phraseRepository.findAll();
+    }
+
+    @Override
+    public PhraseEntity findById(Long id) {
+        Optional<PhraseEntity> phraseEntity = phraseRepository.findById(id);
+        return phraseEntity.orElseThrow(() -> new ObjectNotFoundException("Frase não encontrada"));
+    }
+
+    @Override
+    public PhraseEntity update(Long id, PhraseDTO phraseDTO) {
+
+        Optional<PhraseEntity> phraseEntity = phraseRepository.findById(id);
+
+        phraseEntity.ifPresent(entity -> entity.setPhrase(phraseDTO.getPhrase()));
+
+        return phraseEntity.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
+
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+
+        Optional<PhraseEntity> phraseEntity = phraseRepository.findById(id);
+        if(phraseEntity.isPresent()){
+            phraseRepository.deleteById(id);
+            return true;
+        }
+        else{
+            throw new ObjectNotFoundException("Id não encontrado");
+        }
+
+    }
+
 }
