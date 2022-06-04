cf55ce14ebc1d5674c1b011830bf0d7bcabf0841
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-04 02:39:30 -0300

    -> Criação dos métodos de crud na interface PhraseDAO

--- src/main/java/br/com/frases/services/dao/PhraseDAO.java
+++ src/main/java/br/com/frases/services/dao/PhraseDAO.java
@@ -1,4 +1,16 @@
 package br.com.frases.services.dao;
 
+import br.com.frases.models.dto.PhraseDTO;
+import br.com.frases.models.entity.PhraseEntity;
+
+import java.util.List;
+
 public interface PhraseDAO {
+
+    PhraseEntity create(PhraseDTO phraseDTO);
+    List<PhraseEntity> findAll();
+    PhraseEntity findById(Long id);
+    PhraseEntity update(PhraseDTO phraseDTO);
+    Boolean delete(PhraseDTO phraseDTO);
+
 }
