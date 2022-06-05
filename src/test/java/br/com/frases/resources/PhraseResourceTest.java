6f91b055070a6b59d27ebbebcbce2e083987b6ac
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-05 01:18:56 -0300

    -> Realizei os testes unitários dos endpoints construídos

--- src/test/java/br/com/frases/resources/PhraseResourceTest.java
+++ src/test/java/br/com/frases/resources/PhraseResourceTest.java
@@ -1,10 +1,55 @@
 package br.com.frases.resources;
 
+import br.com.frases.mocks.PhraseDTOBuilder;
+import br.com.frases.models.dto.PhraseDTO;
+import br.com.frases.services.PhraseService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class PhraseResourceTest {
+
+    @InjectMocks
+    PhraseResource phraseResource;
+
+    @Mock
+    PhraseService phraseService;
+
+    @Test
+    @DisplayName("Should test PhraseResource FindAll method")
+    public void shouldTestPhraseResourceFindAllMethod(){
+
+        List<PhraseDTO> phraseDTOS = new ArrayList<>();
+        phraseDTOS.add(PhraseDTOBuilder.builder().withStaticTimeStamp().build());
+
+        Mockito.when(phraseService.findAllValidation()).thenReturn(phraseDTOS);
+
+        Assertions.assertEquals("<200 OK OK,[PhraseDTO(id=1, timeStamp=2022-06-04T02:06:57.260990900, phrase=" +
+                "Frase de teste)],[]>", phraseResource.findAll().toString());
+
+    }
+
+    @Test
+    @DisplayName("Should test PhraseResource findById method")
+    public void shouldTestPhraseResourceFindByIdMethod(){
+
+        Mockito.when(phraseService.findByIdConverter(Mockito.any()))
+                .thenReturn(PhraseDTOBuilder.builder().withStaticTimeStamp().build());
+
+        Assertions.assertEquals("<200 OK OK,PhraseDTO(id=1, timeStamp=2022-06-04T02:06:57.260990900, " +
+                "phrase=Frase de teste),[]>", phraseResource.findById(1L).toString());
+
+    }
+
 }
--- src/test/java/br/com/frases/services/PhraseServiceTest.java
+++ src/test/java/br/com/frases/services/PhraseServiceTest.java
@@ -1,10 +1,95 @@
 package br.com.frases.services;
 
+import br.com.frases.config.ModelMapperConfig;
+import br.com.frases.mocks.PhraseEntityBuilder;
+import br.com.frases.models.entity.PhraseEntity;
+import br.com.frases.resources.exceptions.ObjectNotFoundException;
+import br.com.frases.services.dao.PhraseDAOImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class PhraseServiceTest {
+
+    @InjectMocks
+    PhraseService phraseService;
+
+    @Mock
+    PhraseDAOImpl phraseDAO;
+
+    @Mock
+    ModelMapperConfig modelMapper;
+
+    @Test
+    @DisplayName("Should test PhraseService FindAllValidation method with success")
+    public void shouldTestPhraseServiceFindAllValidationMethodWithSuccess(){
+
+        List<PhraseEntity> phraseEntities = new ArrayList<>();
+        phraseEntities.add(PhraseEntityBuilder.builder().withStaticTimeStamp().build());
+
+        Mockito.when(phraseDAO.findAll()).thenReturn(phraseEntities);
+        Mockito.when(modelMapper.mapper()).thenReturn(new ModelMapper());
+
+        Assertions.assertEquals("[PhraseDTO(id=1, timeStamp=2022-06-04T02:06:57.260990900, phrase=Frase de " +
+                "teste)]", phraseService.findAllValidation().toString());
+
+    }
+
+    @Test
+    @DisplayName("Should test PhraseService FindAllValidation method with ObjectNotFoundException throw")
+    public void shouldTestPhraseServiceFindAllValidationMethodWithExceptionThrow(){
+
+        try{
+            Mockito.when(phraseDAO.findAll()).thenReturn(new ArrayList<>());
+            phraseService.findAllValidation();
+            Assertions.fail();
+        }
+        catch(ObjectNotFoundException e){
+            Assertions.assertEquals("Não existe nenhuma frase cadastrada", e.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Should test PhraseService findByIdConverter method with success")
+    public void shouldTestPhraseServiceFindByIdConverterWithSuccess(){
+
+        Mockito.when(modelMapper.mapper())
+                .thenReturn(new ModelMapper());
+        Mockito.when(phraseDAO.findById(Mockito.any()))
+                .thenReturn(PhraseEntityBuilder.builder().withStaticTimeStamp().build());
+
+        Assertions.assertEquals("PhraseDTO(id=1, timeStamp=2022-06-04T02:06:57.260990900, phrase=Frase de " +
+                "teste)", phraseService.findByIdConverter(1L).toString());
+
+    }
+
+    @Test
+    @DisplayName("Should test PhraseService findByIdConverter method with ObjectNotFoundException throw")
+    public void shouldTestPhraseServiceFindByIdConverterWithObjectNotFoundExceptionThrow(){
+
+        try{
+            Mockito.when(modelMapper.mapper()).thenReturn(new ModelMapper());
+            Mockito.when(phraseDAO.findById(Mockito.any())).thenThrow(ObjectNotFoundException.class);
+            phraseService.findByIdConverter(1L);
+            Assertions.fail();
+        }
+        catch(ObjectNotFoundException e){
+            Assertions.assertNull(e.getMessage());
+        }
+
+    }
+
 }
