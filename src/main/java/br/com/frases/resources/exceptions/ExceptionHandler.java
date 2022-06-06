606dd9258b7508c32065c3e4cf296a5d3a4c9123
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-06 10:26:39 -0300

    -> Criação de exception para inserção de frase nula ou vazia

--- src/main/java/br/com/frases/resources/exceptions/ExceptionHandler.java
+++ src/main/java/br/com/frases/resources/exceptions/ExceptionHandler.java
@@ -1,7 +1,9 @@
 package br.com.frases.resources.exceptions;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -20,5 +22,24 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<StandartError> nullPointerException(NullPointerException nullPointerException,
+                                                              HttpServletRequest httpServletRequest){
+
+        StandartError error = new StandartError(LocalDateTime.now(), 400, nullPointerException.getMessage(),
+                httpServletRequest.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandartError> httpMessageNotReadableException(HttpServletRequest httpServletRequest){
+
+        StandartError error = new StandartError(LocalDateTime.now(), 400, "Teste",
+                httpServletRequest.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
---
+++ src/main/java/br/com/frases/resources/exceptions/NullPointerException.java
@@ -0,0 +1,9 @@
+package br.com.frases.resources.exceptions;
+
+public class NullPointerException extends RuntimeException{
+
+    public NullPointerException(String message) {
+        super(message);
+    }
+
+}
